4f417a4968af817425272de23e7927ab9209d5b4
Aibulat Hakimov <khbulat0203@gmail.com>
2022-04-28 19:58:57 +0400

    Проведен code review

--- Index.java
+++ Index.java
@@ -7,11 +7,13 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.stream.Stream;
 
-
 public class Index {
-    TreeMap<String, List<Pointer>> invertedIndex;
+    /*
+    Внутренние переменные классы желательно делать закрытыми к изменениям извне
+     */
+    private final TreeMap<String, List<Pointer>> invertedIndex;
 
-    ExecutorService pool;
+    private final ExecutorService pool;
 
     public Index(ExecutorService pool) {
         this.pool = pool;
@@ -19,11 +21,11 @@ public class Index {
     }
 
     public void indexAllTxtInPath(String pathToDir) throws IOException {
-        Path of = Path.of(pathToDir);
+        Path path = Path.of(pathToDir);
 
         BlockingQueue<Path> files = new ArrayBlockingQueue<>(2);
 
-        try (Stream<Path> stream = Files.list(of)) {
+        try (Stream<Path> stream = Files.list(path)) {
             stream.forEach(files::add);
         }
 
@@ -32,21 +34,32 @@ public class Index {
         pool.submit(new IndexTask(files));
     }
 
-    public TreeMap<String, List<Pointer>> getInvertedIndex() {
+    /*
+    Здесь должна быть коллекция SortedMap вместо TreeMap, так как в качестве типа возвращаемого значение нужно ставить
+    интерфейс, а не конкретную реализацию коллекции
+     */
+    public SortedMap<String, List<Pointer>> getInvertedIndex() {
         return invertedIndex;
     }
 
-    public List<Pointer> GetRelevantDocuments(String term) {
+    /*
+    Имя метода должно начинаться с маленькой буквы, согласно camelCase
+     */
+    public List<Pointer> getRelevantDocuments(String term) {
         return invertedIndex.get(term);
     }
 
     public Optional<Pointer> getMostRelevantDocument(String term) {
-        return invertedIndex.get(term).stream().max(Comparator.comparing(o -> o.count));
+        /*
+        Для лучшего вида кода
+         */
+        return invertedIndex.get(term).stream()
+                .max(Comparator.comparing(o -> o.count));
     }
 
     static class Pointer {
         private Integer count;
-        private String filePath;
+        private final String filePath;
 
         public Pointer(Integer count, String filePath) {
             this.count = count;
@@ -55,7 +68,10 @@ public class Index {
 
         @Override
         public String toString() {
-            return "{" + "count=" + count + ", filePath='" + filePath + '\'' + '}';
+            return "{" +
+                    "count=" + count +
+                    ", filePath='" + filePath + '\'' +
+                    "}";
         }
     }
 
@@ -73,29 +89,43 @@ public class Index {
                 Path take = queue.take();
                 List<String> strings = Files.readAllLines(take);
 
-                strings.stream().flatMap(str -> Stream.of(str.split(" "))).forEach(word -> invertedIndex.compute(word, (k, v) -> {
-                    if (v == null) return List.of(new Pointer(1, take.toString()));
-                    else {
-                        ArrayList<Pointer> pointers = new ArrayList<>();
+                strings.stream()
+                        .flatMap(str -> Stream.of(str.split(" ")))
+                        .forEach(word -> invertedIndex.compute(word,
+                                (k, v) -> {
+                                    if (v == null) {
+                                        return List.of(new Pointer(1, take.toString()));
+                                    } else {
+                                        ArrayList<Pointer> pointers = new ArrayList<>();
 
-                        if (v.stream().noneMatch(pointer -> pointer.filePath.equals(take.toString()))) {
-                            pointers.add(new Pointer(1, take.toString()));
-                        }
+                                        if (v.stream().noneMatch(pointer -> pointer.filePath.equals(take.toString()))) {
+                                            pointers.add(new Pointer(1, take.toString()));
+                                        }
 
-                        v.forEach(pointer -> {
-                            if (pointer.filePath.equals(take.toString())) {
-                                pointer.count = pointer.count + 1;
-                            }
-                        });
+                                        v.forEach(pointer -> {
+                                            if (pointer.filePath.equals(take.toString())) {
+                                                pointer.count = pointer.count + 1;
+                                            }
+                                        });
 
-                        pointers.addAll(v);
+                                        pointers.addAll(v);
 
-                        return pointers;
-                    }
-
-                }));
-
+                                        return pointers;
+                                    }
+                                }));
+            /*
+            Исключение Interrupted не должно обрабатываться подобным образом, иначе информация о том, почему поток был
+            прерван будет потеряна и не удастся узнать причин прерывания потока
+             */
             } catch (InterruptedException | IOException e) {
+                /*
+                Чтобы такого не было, нужно дополнительно прервать поток, чтобы исключение не осталось упущенным
+                 */
+                Thread.currentThread().interrupt();
+                /*
+                Не стоит использовать общий класс Runtime Exception при обработке исключения, нужно создать свой для
+                конкретной ошибки и пробрасывать туда хоть какую-то информацию, а не оставлять пустым поле сообщение
+                 */
                 throw new RuntimeException();
             }
         }
